09d84adac2c3ebc931c9021afa183b16896d377a
deepthi7143 <150417779+deepthi7143@users.noreply.github.com>
2024-02-16 17:17:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import *
 
 app = Flask(__name__)
@@ -8,4 +7,5 @@ def home():
     return "<h1>Welcome to Jenkins Tutorials<h1/>"
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0'
+    app.run(debug=True,host='0.0.0.0')
+
